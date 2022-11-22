bf66c2aa633bf6c50b014036a67cb96947f37ea7
Rimon Moomey <rimon.moomey@gmail.com>
2022-11-22 11:25:06 -0600

    complete state lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [ inCart, toggleInCart ] = useState(false);
+
+  const addHandle = () => {
+    toggleInCart(!inCart);
+  };
+
+  const liClass = inCart ? "in-cart" : "";
+  const buttonText = inCart ? "Remove from" : "Add to"
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addHandle}>
+        {buttonText} Cart
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, changeCategory] = useState("All");
+
+  const handleChange = (category) => {
+    changeCategory(category);
+  };
+
+  const filter = (items) => {
+    let itemList;
+
+    if (selectedCategory == "All") {
+      itemList = items;
+    } else {
+      itemList = items.filter((item) => item.category == selectedCategory);
+    }
+
+    const itemLIList = itemList.map((item) => {
+      return <Item key={item.id} name={item.name} category={item.category} />;
+    });
+
+    return itemLIList;
+  };
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select
+          name="filter"
+          value={selectedCategory}
+          onChange={(e) => handleChange(e.target.value)}
+        >
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
-      <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
-      </ul>
+      <ul className="Items">{filter(items)}</ul>
     </div>
   );
 }
